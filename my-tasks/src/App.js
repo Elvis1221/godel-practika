2bea58f7fddd1651d07980ae2f232faea3c02ad2
Andrei <fujitsuelvis@gmail.com>
2020-12-28 15:05:59 +0300

    task 1

--- my-tasks/src/App.js
+++ my-tasks/src/App.js
@@ -1,7 +1,8 @@
+import React from 'react'
 import './App.css';
 
-import {block1, block2, block3} from "./components/task1/newspaper-blocks";
-import {Newspaper} from "./components/task1/Newspaper,jsx";
+import {newspaperBlock} from "./components/task1/newspaper-blocks";
+import {Newspaper} from "./components/task1/Newspaper";
 
 
 function App() {
@@ -10,14 +11,9 @@ function App() {
         <div className="App">
 
             {
-                block1.map((item) => <Newspaper heading={item.heading} subtitle={item.subtitle} article={item.article}/>)
+                newspaperBlock.map((item) => <Newspaper heading={item.heading} subtitle={item.subtitle} article={item.article}/>)
             }
-            {
-                block2.map((item) => <Newspaper heading={item.heading} subtitle={item.subtitle} article={item.article}/>)
-            }
-            {
-                block3.map((item) => <Newspaper heading={item.heading} subtitle={item.subtitle} article={item.article}/>)
-            }
+
         </div>
     );
 }
--- my-tasks/src/components/task1/newspaper-blocks.jsx
+++ my-tasks/src/components/task1/newspaper-blocks.jsx
@@ -1,16 +1,23 @@
-export const block1 = [
-    {heading: 'Заголовок'},
-    {subtitle: 'Подзаголовок'},
-    {article: 'Статья'},
+import React from 'react'
+
+export const newspaperBlock = [
+    {
+        heading: 'Заголовок',
+        subtitle: 'Подзаголовок',
+        article: 'Статья',
+    },
+    {
+        heading: 'Заголовок',
+        subtitle: 'Подзаголовок',
+        article: 'Статья',
+    },
+    {
+        heading: 'Заголовок',
+        subtitle: 'Подзаголовок',
+        article: 'Статья',
+    }
+
+
 ];
-export const block2 = [
-    {heading: 'Заголовок2'},
-    {subtitle: 'Подзаголовок2'},
-    {article: 'Статья2'},
-];
-export const block3 = [
-    {heading: 'Заголовок3'},
-    {subtitle: 'Подзаголовок3'},
-    {article: 'Статья3'},
-];
+
 
